ab82416fed03e70dd0ce63f902e09b345bcee6cb
Luis Enrique Gomez Molina <louhenry@gmail.com>
2025-06-03 17:15:33 -0600

    Revert "20250603_01"
    This reverts commit ba70f7114377f38f8e30cb22068bdffb8c323a33.

--- Modulo 2 - Variables/Variables/Codigo/03OtrosNumerosEnteros.cs
+++ Modulo 2 - Variables/Variables/Codigo/03OtrosNumerosEnteros.cs
@@ -7,23 +7,23 @@ using System.Threading.Tasks;
 
 namespace Variables.Codigo
 {
-    internal class OtrosNumerosEnteros
-    {
-        public void CodigoDelCurso()
-        {
-            int distanciaDeLaTierraAlSolEnKilometros = 150_000_000; // -2,147,483,648 hasta 2,147,483,647
-            uint uint2 = 3_000_000_000; // 0 hasta 4,294,967,295
+internal class OtrosNumerosEnteros
+{
+public void CodigoDelCurso()
+{
+int distanciaDeLaTierraAlSolEnKilometros = 150_000_000; // -2,147,483,648 hasta 2,147,483,647
+uint uint2 = 3_000_000_000; // 0 hasta 4,294,967,295
 
-            long long1 = 8_000_000_000_000; // -9,223,372,036,854,775,808 hasta 9,223,372,036,854,775,807
-            ulong ulong1 = 16_000_000_000_000; // 0 hasta 18,446,744,073,709,551,615
+long long1 = 8_000_000_000_000; // -9,223,372,036,854,775,808 hasta 9,223,372,036,854,775,807
+ulong ulong1 = 16_000_000_000_000; // 0 hasta 18,446,744,073,709,551,615
 
-            byte byte1 = 145; // 0 hasta 255
-            sbyte sbyte1 = 100; // -128 hasta 127
+byte byte1 = 145; // 0 hasta 255
+sbyte sbyte1 = 100; // -128 hasta 127
 
-            short short1 = 30_000; // -32,768 hasta 32,767
-            ushort ushort1 = 65_000; // 0 hasta 65,535
-        }
-    }
+short short1 = 30_000; // -32,768 hasta 32,767
+ushort ushort1 = 65_000; // 0 hasta 65,535
+}
+}
 }
 
 #pragma warning restore CS0219 // Variable is assigned but its value is never used
--- Modulo 2 - Variables/Variables/Codigo/04NumerosPuntoFlotantes.cs
+++ Modulo 2 - Variables/Variables/Codigo/04NumerosPuntoFlotantes.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace Variables.Codigo
 {
-    public class NumerosPuntoFlotantes
-    {
-        public void CodigoDelCurso()
-        {
-            float altura = 180.123456789f; // Ocupa 4 bytes; Precisión de 6 a 9 dígitos
-            double anchura = 45.123456789; // Ocupa 8 bytes; Precisión de 15 a 17 dígitos
-            decimal monto = 9.99m; // Ocupa 16 bytes; Precisión de 28 a 29 dígitos (es recomendado temas financieros)
+public class NumerosPuntoFlotantes
+{
+public void CodigoDelCurso()
+{
+float altura = 180.123456789f; // Ocupa 4 bytes; Precisión de 6 a 9 dígitos
+double anchura = 45.123456789; // Ocupa 8 bytes; Precisión de 15 a 17 dígitos
+decimal monto = 9.99m; // Ocupa 16 bytes; Precisión de 28 a 29 dígitos (es recomendado temas financieros)
 
-            Console.WriteLine("La altura es:");
-            Console.WriteLine(altura);
+Console.WriteLine("La altura es:");
+Console.WriteLine(altura);
 
-            Console.WriteLine("La anchura es:");
-            Console.WriteLine(anchura);
+Console.WriteLine("La anchura es:");
+Console.WriteLine(anchura);
 
-            Console.WriteLine("El precio es:");
-            Console.WriteLine(monto);
+Console.WriteLine("El precio es:");
+Console.WriteLine(monto);
 
-            Console.ReadLine();
-        }
-    }
+Console.ReadLine();
+}
+}
 }
