ba70f7114377f38f8e30cb22068bdffb8c323a33
Luis Enrique Gomez Molina <louhenry@gmail.com>
2025-06-03 16:32:33 -0600

    20250603_01

--- Modulo 2 - Variables/Variables/Codigo/01SintaxisBasicaCSharp.cs
+++ Modulo 2 - Variables/Variables/Codigo/01SintaxisBasicaCSharp.cs
@@ -6,32 +6,32 @@ using System.Threading.Tasks;
 
 namespace Variables.Codigo
 {
-internal class SintaxisBasicaCSharp
-{
-public void CodigoDelCurso()
-{
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+    internal class SintaxisBasicaCSharp
+    {
+        public void CodigoDelCurso()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Yo soy Claudia; Mi edad es 123");
+            Console.WriteLine("Yo soy Claudia; Mi edad es 123");
 
-Console.WriteLine("Lo que yo quiera");
+            Console.WriteLine("Lo que yo quiera");
 
-// Este es un ejemplo de un bloque de código
-{
-Console.WriteLine("Este código está en un bloque");
-}
+            // Este es un ejemplo de un bloque de código
+            {
+                Console.WriteLine("Este código está en un bloque");
+            }
 
-// Este código permite escribir en la consola
+            // Este código permite escribir en la consola
 
-/*
-Tienes un comentario
-de 
-varias líneas
-*/
+            /*
+            Tienes un comentario
+            de 
+            varias líneas
+            */
 
 
-Console.WriteLine("Este código no está dentro de un bloque");
-}
-}
+            Console.WriteLine("Este código no está dentro de un bloque");
+        }
+    }
 }
